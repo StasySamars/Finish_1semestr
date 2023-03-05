78d9d30ea73994c4192ccd1db7b42fa59ba38c81
StasySamars <119619342+StasySamars@users.noreply.github.com>
2023-03-05 12:55:47 +0300

    написан код

--- Program.cs
+++ Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿/* Написать программу, которая из имеющегося массива строк формирует 
+массив из строк, длинна которого меньше либо равна 3 символа. 
+Первоначальный массив можно внести с клавиатуры, либо задать на старте 
+выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
+лучше обойтись исключительно массивами */
+string[] randomStringArray = { "Hello", "2", "world", ":-)", "43", "j34h" };
+
+string[] LimitSymbolArray(string[] array, int numberOfSymbols)
+{
+    int j = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < numberOfSymbols + 1)
+        {
+            j++;
+        }
+    }
+
+    string[] newArray = new string[j];
+    j = 0;  
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length < numberOfSymbols + 1)
+        {
+            newArray[j] = array[i];
+            j++;
+        }
+    }
+    return newArray;
+}
+
+void ShowArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{{{array[i]}}}, ");
+    }
+}
+
+string[] arrayСhoiceLength = LimitSymbolArray(randomStringArray, 3);
+ShowArray(randomStringArray);
+Console.WriteLine();
+ShowArray(arrayСhoiceLength);
